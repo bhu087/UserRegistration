34835d1c1ac39ea233e92403715bd3cd1956300f
bhu087 <bhu087@gmail.com>
2021-05-05 15:08:08 +0530

    [Bhushan] Refactor. Email added

--- UserRegistration/UserValidation.cs
+++ UserRegistration/UserValidation.cs
@@ -20,7 +20,7 @@ namespace UserRegistration
         }
         public bool ValidateEmail(string email)
         {
-            return Regex.IsMatch(email, @"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$");
+            return Regex.IsMatch(email, EMAIL_REGEX);
         }
     }
 }
