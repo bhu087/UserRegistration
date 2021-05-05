758245383f585e23328fb86a53c037bb4e1a98e0
bhu087 <bhu087@gmail.com>
2021-05-05 15:42:49 +0530

    [Bhushan] Refactor. Password(atleast 1 Numeric value) case added(UC6)

--- UserRegistration/UserValidation.cs
+++ UserRegistration/UserValidation.cs
@@ -11,7 +11,7 @@ namespace UserRegistration
         const string LAST_NAME_REGEX = FIRST_NAME_REGEX;
         const string EMAIL_REGEX = @"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$";
         const string MOBILE_NUMBER_REGEX = @"^[0-9]{2}[\s][6-9]{1}[0-9]{9}";
-        const string PASSWORD_REGEX = @"^(?=.*?[A-Z]).{8,}$";
+        const string PASSWORD_REGEX = @"^(?=.*?[A-Z])(?=.*?[0-9]).{8,}$";
         public bool ValidateFirstName(string firstName)
         {
             return Regex.IsMatch(firstName,FIRST_NAME_REGEX);
