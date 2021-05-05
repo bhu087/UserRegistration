c9925a8123e5a2e36f4c79402d42d7152ed3160d
bhu087 <bhu087@gmail.com>
2021-05-05 14:34:39 +0530

    [Bhushan] Refactor. First name added

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -6,7 +6,15 @@ namespace UserRegistration
     {
         static void Main(string[] args)
         {
+            UserValidation userValidation = new UserValidation();
             Console.WriteLine("Welcome to User registration");
+            Console.WriteLine("Enter First Name");
+            string firstName = Console.ReadLine();
+            while (!userValidation.ValidateFirstName(firstName))
+            {
+                Console.WriteLine("Enter Valid Name");
+                firstName = Console.ReadLine();
+            }
         }
     }
 }
---
+++ UserRegistration/UserValidation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    class UserValidation
+    {
+        const string FIRST_NAME_REGEX = "^[A-Z]{1}[a-z]{2,}$";
+        public bool ValidateFirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName,FIRST_NAME_REGEX);
+        }
+    }
+}
